4c29e964f4a9ad33c91bfe013c40831d65267dce
GuilhermeRocha75 <guilhermeecheverritodarocha@gmail.com>
2024-05-28 17:08:54 -0300

    exercicio9

---
+++ ExerciciosDOM/exercicio9/script.js
@@ -0,0 +1,9 @@
+function esconderImagem() {
+    var img = document.getElementById('imagem');
+    img.style.display = 'none';
+  }
+
+  function mostrarImagem() {
+    var img = document.getElementById('imagem');
+    img.style.display = 'block';
+  }
